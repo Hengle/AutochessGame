56a2d27e384ef311f766de6eb6e8a6d945229ee2
caplanmaor <caplanmaor@gmail.com>
2019-08-28 08:43:40 +0300

    UI Base Assets

---
+++ Untitled Auto Chess Game/Assets/Team/Maor/GUI/Scripts/ItemSlots.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlots : MonoBehaviour
+{
+
+    public Item Item;
+    public Image Image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnValidate()
+    {
+        if (Image == null)
+        {
+            Image = GetComponent<Image>();
+        }
+    }
+}
